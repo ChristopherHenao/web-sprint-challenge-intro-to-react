d2d9c73ae105f14b21a985c6910178ff0b9e049a
ChristoherHenao <cq.henao@gmail.com>
2021-05-14 12:44:26 -0400

    API get within useEffect in app page done

--- src/App.js
+++ src/App.js
@@ -1,19 +1,35 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
 import Character from './components/Character';
+import axios from 'axios';
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
+  const [characterData, setCharacterData] = useState(null);
+  console.log(characterData)
 
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
+  useEffect( () => {
+    axios.get('https://swapi.dev/api/people')
+    .then(function (response) {
+      // handle success
+      setCharacterData(response.data);
+    })
+    .catch(function (error) {
+      // handle error
+      console.log(error);
+    })
+  },[])
+
+
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
-      <Character/>
+      <Character characterData={characterData}/>
     </div>
   );
 }
