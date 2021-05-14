4e82feb22414dfef8e5889eb837d1441fbb25af5
ChristoherHenao <cq.henao@gmail.com>
2021-05-14 14:03:57 -0400

    mvp done

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ import axios from 'axios';
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
-  const [characterData, setCharacterData] = useState(null);
-  console.log(characterData)
+  const [characterData, setCharacterData] = useState([]);
+  
 
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
@@ -29,7 +29,9 @@ const App = () => {
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
-      <Character characterData={characterData}/>
+      {characterData.map(character =>{
+        return <Character character={character} />
+      })}
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,13 +1,32 @@
 // Write your Character component here
 import React from 'react';
+import styled from "styled-components"
 
-const Character = () => {
+const StyledCharacter = styled.div`
+    /* color:blue; */
+    background-color: lightskyblue;
+    border-radius: 30px;
+    border: 2px solid black;
+    width: 50%;
+    margin: 2% auto;
+`;
+
+const Character = (props) => {
+    
+    const { character } = props;
+    console.log(character)
 
     return (
-        <div>
-            This is where Character will be
-        </div>
-    )
+        <StyledCharacter>
+            <h3>Name: {character.name}</h3>
+            <h4>Height: {character.height}</h4>
+            <h4>Mass: {character.mass}</h4>
+            <h4>Skin color: {character.skin_color}</h4>
+            <h4>Eye color: {character.eye_color}</h4>
+        </StyledCharacter>
+    ) 
+    
 }
 
 export default Character;
+
