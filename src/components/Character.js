6bcc87afdce6f3ccd2de6deec186083fd5f142ad
ChristoherHenao <cq.henao@gmail.com>
2021-05-14 12:27:39 -0400

    character page linked to app page done

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,13 @@
 // Write your Character component here
+import React from 'react';
+
+const Character = () => {
+
+    return (
+        <div>
+            This is where Character will be
+        </div>
+    )
+}
+
+export default Character;
