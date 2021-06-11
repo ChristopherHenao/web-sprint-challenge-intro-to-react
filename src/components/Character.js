6d60d5cdcd970ce87f6c4814266d0c7c32db309d
ChristoherHenao <cq.henao@gmail.com>
2021-06-11 13:42:15 -0400

    use state & get request done

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,14 @@
 // Write your Character component here
+import React from 'react'
+
+
+const Character = () => {
+
+    return (
+        <div>
+            Character
+        </div>
+    )
+}
+
+export default Character
